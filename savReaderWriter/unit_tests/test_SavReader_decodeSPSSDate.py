0d0ab22d7067c4f995f716bf6a6d32f728c4d2d4
fomcl <fomcl@yahoo.com>
2016-09-12 19:18:32 +0200

    Fixed test_SavReader_decodeSPSSDate.py so it is skipped on Windows, which does not has a locale C.UTF-8

--- savReaderWriter/unit_tests/test_SavReader_decodeSPSSDate.py
+++ savReaderWriter/unit_tests/test_SavReader_decodeSPSSDate.py
@@ -9,14 +9,19 @@ from savReaderWriter import SavReader, SavWriter
 
 
 # issue 52 "Incorrect date decoding in Python 3"
-class DecodeSPSSDateTest(unittest.TestCase):
-    def test(self):
-        with tempfile.NamedTemporaryFile(suffix='.sav') as tmpfile:
-            with SavWriter(tmpfile.name, ['date'], {b'date': 0}, formats={b'date': b'EDATE40'}, ioUtf8=True, ioLocale="C.UTF-8") as writer:
-                writer.writerow([writer.spssDateTime(b"2000-01-01", "%Y-%m-%d")])
+class test_SavReader_decode_SPSSDate(unittest.TestCase):
 
-            with SavReader(tmpfile.name, returnHeader=False, ioUtf8=True, ioLocale="C.UTF-8") as reader:
-                print(sys.version_info)
+    @unittest.skipIf(sys.platform.startswith("win"), "'No C.UTF-8' on Windows")
+    def test_date_conversion(self):
+        with tempfile.NamedTemporaryFile(suffix='.sav') as tmpfile:
+            with SavWriter(tmpfile.name, ['date'], {b'date': 0}, 
+                           formats={b'date': b'EDATE40'}, ioUtf8=True, 
+                           ioLocale="C.UTF-8") as writer:
+                record = [writer.spssDateTime(b"2000-01-01", "%Y-%m-%d")]
+                writer.writerow(record)
+
+            with SavReader(tmpfile.name, returnHeader=False, ioUtf8=True, 
+                           ioLocale="C.UTF-8") as reader:
                 date = list(reader)[0][0]
                 self.assertEqual('2000-01-01', date)
 
